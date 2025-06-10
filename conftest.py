0f64ef8bd333360382be1b5d9c76687e980b247d
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-06-10 14:17:29 +0530

    conftest.py

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
+#importing the needed packages
 import pytest
 from selenium import webdriver
 
 
-@pytest.fixture
+@pytest.fixture 
 def driver():
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.get("https://www.guvi.in/")
+    
+    chrome_driver = webdriver.Chrome() #To open the browser
+    
+    chrome_driver.get("https://www.guvi.in/")  #to open the url(webpage)
 
     return chrome_driver
 
